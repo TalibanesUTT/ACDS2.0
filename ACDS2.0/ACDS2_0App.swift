d5114f8615c34df53764590a9a9edc95f7091bab
Luis Angel Zapata <dev.luis.zapata@gmail.com>
2024-07-29 21:51:10 -0600

    Initial Commit

---
+++ ACDS2.0/ACDS2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  ACDS2_0App.swift
+//  ACDS2.0
+//
+//  Created by Luis Angel Zapata Zuñiga on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ACDS2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
